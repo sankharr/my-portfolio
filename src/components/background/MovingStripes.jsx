f81cf093e51e8ed254f2f11fb6120c3077ca138f
Sankha Rathnayake <2017cs143@stu.ucsc.cmb.ac.lk>
2022-08-15 22:16:40 +0530

    MovingStripes added

---
+++ src/components/background/MovingStripes.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './MovingStripes.scss';
+
+function MovingStripes() {
+  return (
+    <div className='MovingStripes'></div>
+  )
+}
+
+export default MovingStripes
